93ce275a34d2ad2f7959bcdcf9b0efa8428280b5
Caroline Jaffré <carolinemljaffre@gmail.com>
2026-06-01 20:50:36 +0200

    feat: tests for Japanese

--- tests/shared.py
+++ tests/shared.py
@@ -4,10 +4,13 @@ from pathlib import Path
 MOCK_DIR = Path(__file__).parent / "mock"
 MOCK_EPUB_TW = MOCK_DIR / "book_zh-TW.epub"
 MOCK_EPUB_CN = MOCK_DIR / "book_zh-CN.epub"
+MOCK_EPUB_JA = MOCK_DIR / "book_ja.epub"
 MOCK_TXT_TW  = MOCK_DIR / "book_zh-TW.txt"
 MOCK_TXT_CN  = MOCK_DIR / "book_zh-CN.txt"
+MOCK_TXT_JA  = MOCK_DIR / "book_ja.txt"
 MOCK_SRT_CN = MOCK_DIR / "srt_zh-CN.srt"
 MOCK_SRT_TW = MOCK_DIR / "srt_zh-TW.srt"
+MOCK_SRT_JA = MOCK_DIR / "srt_ja.srt"
 
 skip_if_no_epub_tw = pytest.mark.skipif(
     not MOCK_EPUB_TW.exists(),
@@ -17,6 +20,10 @@ skip_if_no_epub_cn = pytest.mark.skipif(
     not MOCK_EPUB_CN.exists(),
     reason="tests/mock/book_zh-CN.epub not available"
 )
+skip_if_no_epub_ja = pytest.mark.skipif(
+    not MOCK_EPUB_JA.exists(),
+    reason="tests/mock/book_ja.epub not available"
+)
 skip_if_no_txt_tw = pytest.mark.skipif(
     not MOCK_TXT_TW.exists(),
     reason="tests/mock/book_zh-TW.txt not available"
@@ -25,6 +32,10 @@ skip_if_no_txt_cn = pytest.mark.skipif(
     not MOCK_TXT_CN.exists(),
     reason="tests/mock/book_zh-CN.txt not available"
 )
+skip_if_no_txt_ja = pytest.mark.skipif(
+    not MOCK_TXT_JA.exists(),
+    reason="tests/mock/book_ja.txt not available"
+)
 skip_if_no_srt_cn = pytest.mark.skipif(
     not MOCK_SRT_CN.exists(),
     reason="tests/mock/srt_zh-CN.srt not available"
@@ -32,4 +43,8 @@ skip_if_no_srt_cn = pytest.mark.skipif(
 skip_if_no_srt_tw = pytest.mark.skipif(
     not MOCK_SRT_TW.exists(),
     reason="tests/mock/srt_zh-TW.srt not available"
+)
+skip_if_no_srt_ja = pytest.mark.skipif(
+    not MOCK_SRT_JA.exists(),
+    reason="tests/mock/srt_ja.srt not available"
 )
